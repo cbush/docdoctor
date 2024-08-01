2dbc93b4a10c66cc5e1d0c38e3ca7d9892a1460c
Chris Bush <chris.bush@mongodb.com>
2024-08-01 17:28:24 -0400

    WIP

--- src/commands/findNested.ts
+++ src/commands/findNested.ts
@@ -1,8 +1,10 @@
+import fetch from "node-fetch";
 import { CommandModule } from "yargs";
 import { createInterface } from "readline";
 import { findAll } from "../tree";
 import { ParentNode } from "restructured";
-import fetch from "node-fetch";
+import { stringify } from "csv-stringify/sync";
+import { makeSnootyProjectsInfo } from "../SnootyProjectsInfo";
 
 // These types are what's in the snooty manifest jsonl file.
 export type SnootyManifestEntry = {
@@ -27,16 +29,18 @@ export type SnootyPageData = {
   deleted: boolean;
 };
 
-const defaultDataApiBaseUrl = "https://snooty-data-api.mongodb.com/prod/";
+const defaultSnootyDataApiBaseUrl = "https://snooty-data-api.mongodb.com/prod/";
 
-const fetchPageData = async (
-  args: FindNestedArgs
-): Promise<SnootyPageData[]> => {
-  const { dataApiBaseUrl, project, branch } = args;
+const fetchPageData = async (args: {
+  branchName: string;
+  projectName: string;
+  snootyDataApiBaseUrl: string;
+}): Promise<SnootyPageData[]> => {
+  const { snootyDataApiBaseUrl, projectName, branchName } = args;
 
   const getBranchDocumentsUrl = new URL(
-    `projects/${project}/${branch}/documents`,
-    dataApiBaseUrl ?? defaultDataApiBaseUrl
+    `projects/${projectName}/${branchName}/documents`,
+    snootyDataApiBaseUrl ?? defaultSnootyDataApiBaseUrl
   );
   const { body } = await fetch(getBranchDocumentsUrl);
   if (body === null) {
@@ -57,8 +61,8 @@ const fetchPageData = async (
           pages.push(data);
           return;
         }
-        case "asset":
         case "metadata":
+        case "asset":
         case "timestamp":
           // Nothing to do with these
           return;
@@ -85,16 +89,20 @@ type DirectiveNode = ParentNode & {
 };
 
 const findDirectives = (ast: ParentNode) => {
-  return findAll(ast, ({ type }) => type === "directive") as DirectiveNode[];
+  return (
+    findAll(ast, ({ type }) => type === "directive") as DirectiveNode[]
+  ).filter((node) => node !== ast); // exclude self
 };
 
-const findDirectivesNamed = (ast: ParentNode, named: string) =>
-  findDirectives(ast).filter(({ name }) => name === named);
+const findDirectivesNamed = (ast: ParentNode, named: string | string[]) => {
+  const names = Array.isArray(named) ? named : [named];
+  return findDirectives(ast).filter(({ name }) => names.includes(name));
+};
 
 const countXNestedInY = (
   ast: ParentNode,
-  xDirectiveName: string,
-  yDirectiveName: string
+  xDirectiveName: string | string[],
+  yDirectiveName: string | string[]
 ) => {
   return findDirectivesNamed(ast, yDirectiveName).reduce(
     (acc, cur) => acc + findDirectivesNamed(cur, xDirectiveName).length,
@@ -102,18 +110,56 @@ const countXNestedInY = (
   );
 };
 
-const countNestedDirectives = (ast: ParentNode, directiveName: string) =>
-  countXNestedInY(ast, directiveName, directiveName);
+const countNestedDirectives = (
+  ast: ParentNode,
+  directiveName: string | string[]
+) => countXNestedInY(ast, directiveName, directiveName);
 
 type OffenseCounter = (ast: ParentNode) => number;
 
+enum DirectiveNames {
+  TABS = "tabs",
+  TABLE = "list-table",
+  PROCEDURE = "procedure",
+  EXAMPLE = "example", // Example is a directive but they want it tracked separately
+}
+
+const ADMONITION_TYPE_DIRECTIVE_NAMES = [
+  "admonition",
+  "note",
+  "warning",
+  "important",
+  "danger",
+  "caution",
+  "tip",
+  "see",
+  "seealso",
+];
+
 const offenseCounters: Record<string, OffenseCounter> = {
-  nestedTabs: (ast) => countNestedDirectives(ast, "tab"),
-  tablesInTables: (ast) => countNestedDirectives(ast, "table"),
-  admonitionsInAdmonitions: (ast) => countNestedDirectives(ast, "admonition"),
-  proceduresInProcedures: (ast) => countNestedDirectives(ast, "procedure"),
-  proceduresInTabs: (ast) => countXNestedInY(ast, "procedure", "tab"),
-  admonitionsInTables: (ast) => countXNestedInY(ast, "admonition", "table"),
+  nestedTabs: (ast) => countNestedDirectives(ast, DirectiveNames.TABS),
+  tablesInTables: (ast) => countNestedDirectives(ast, DirectiveNames.TABLE),
+  admonitionsInAdmonitions: (ast) =>
+    countNestedDirectives(ast, ADMONITION_TYPE_DIRECTIVE_NAMES),
+  admonitionsInTables: (ast) =>
+    countXNestedInY(ast, ADMONITION_TYPE_DIRECTIVE_NAMES, DirectiveNames.TABLE),
+  proceduresInProcedures: (ast) =>
+    countNestedDirectives(ast, DirectiveNames.PROCEDURE),
+  proceduresInNestedTabs: (ast) =>
+    findDirectivesNamed(ast, DirectiveNames.TABS).reduce(
+      (acc, cur) =>
+        acc +
+        countXNestedInY(cur, DirectiveNames.PROCEDURE, DirectiveNames.TABS),
+      0
+    ),
+  examplesInTables: (ast) =>
+    countXNestedInY(ast, DirectiveNames.EXAMPLE, DirectiveNames.TABLE),
+  examplesInAdmonitions: (ast) =>
+    countXNestedInY(
+      ast,
+      DirectiveNames.EXAMPLE,
+      ADMONITION_TYPE_DIRECTIVE_NAMES
+    ),
 };
 
 type FindNestedResult = {
@@ -134,47 +180,112 @@ const findNested = (pageData: SnootyPageData): FindNestedResult => {
 };
 
 type FindNestedArgs = {
-  dataApiBaseUrl?: string;
-  project: string;
-  branch: string;
+  snootyDataApiBaseUrl?: string;
+  branch?: string;
+  repo: string;
+};
+
+const makeGitHubBaseUrl = async ({
+  repoName,
+  branchName,
+}: {
+  repoName: string;
+  branchName: string;
+}) => {
+  const makeUrl = (org: string) =>
+    `https://github.com/${org}/${repoName}/blob/${branchName}/source/`;
+
+  const mongoDbUrl = makeUrl("mongodb");
+  const probe = await fetch(mongoDbUrl);
+  if (probe.status === 404) {
+    return makeUrl("10gen");
+  }
+  return mongoDbUrl;
 };
 
 const commandModule: CommandModule<unknown, FindNestedArgs> = {
   command: "findNested",
   builder(args) {
     return args
-      .option("dataApiBaseUrl", {
+      .option("snootyDataApiBaseUrl", {
         array: false,
         type: "string",
         demandOption: false,
       })
-      .option("project", { type: "string", array: false, demandOption: true })
-      .option("branch", { type: "string", array: false, demandOption: true });
+      .option("repo", {
+        type: "string",
+        array: false,
+        demandOption: true,
+      })
+      .option("branch", { type: "string", array: false, demandOption: false });
   },
   handler: async (args) => {
     try {
-      const { branch, project, dataApiBaseUrl } = args;
-      const pageData = await fetchPageData({ branch, project, dataApiBaseUrl });
-      /*
-      pageData.forEach((page) => {
-        const result = findNested(page);
-        console.log(JSON.stringify(result));
+      const {
+        branch,
+        repo: repoName,
+        snootyDataApiBaseUrl,
+      } = {
+        ...args,
+        snootyDataApiBaseUrl:
+          args.snootyDataApiBaseUrl ?? defaultSnootyDataApiBaseUrl,
+      };
+
+      const snootyProjectsInfo = await makeSnootyProjectsInfo({
+        snootyDataApiBaseUrl,
       });
-      */
+
+      const projectName = await snootyProjectsInfo.getProjectName({ repoName });
+
+      if (projectName === undefined) {
+        throw new Error(`Couldn't find project for repo '${repoName}'!`);
+      }
+
+      const branchName =
+        branch ??
+        (await snootyProjectsInfo.getCurrentVersionName({
+          projectName,
+        })) ??
+        "master";
+
+      const baseUrl = await snootyProjectsInfo.getBaseUrl({
+        projectName,
+        branchName,
+      });
+
+      const pageData = await fetchPageData({
+        branchName,
+        projectName,
+        snootyDataApiBaseUrl,
+      });
+
+      const repoBaseUrl = await makeGitHubBaseUrl({ repoName, branchName });
+
+      const results = pageData
+        .map((page) => findNested(page))
+        .filter(
+          ({ counts }) =>
+            Object.values(counts).reduce((acc, cur) => acc + cur, 0) > 0 // Only include results if any count > 0
+        );
       console.log(
-        JSON.stringify(
-          pageData
-            .map(findNested)
-            .reduce(
-              (acc, cur) =>
-                Object.fromEntries(
-                  Object.entries(cur.counts).map(([k, v]) => [
-                    k,
-                    v + (acc[k] ?? 0),
-                  ])
-                ),
-              {} as Record<string, number>
-            )
+        stringify(
+          results.map(({ counts, pageId }) => ({
+            repoName,
+            projectName,
+            branchName,
+            pageId: pageId.replace(
+              `${projectName}/docsworker-xlarge/${branchName}/`,
+              baseUrl.replace(/\/?$/, "/")
+            ),
+            pageSource: `${pageId.replace(
+              `${projectName}/docsworker-xlarge/${branchName}/`,
+              repoBaseUrl
+            )}.txt`,
+            ...counts,
+          })),
+          {
+            header: true,
+          }
         )
       );
     } catch (error) {
